5f2f0f5d36fed572f5652bee1631ddb94edbd205
Elizabeth Clawson <emclawson1@gmail.com>
2024-09-03 15:16:43 -0500

    end of class

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
+app.use(express.static("./styles"));
+
+const fs = require("fs");
+
+app.engine("stuff", (filePath, options, callback) => {
+    fs.readFile(filePath, (err, content) => {
+        if (err) return callback(err);
+   
+    const rendered = content 
+        .toString()
+        .replaceAll("#title#", `${options.title}`)
+        .replace("#content#", `${options.content}`)
+        .replace("#info#", `${options.info}`);
+    return callback(null, rendered);
+    });
+});
+app.set("views", "./stuff");
+app.set("view engine", "stuff");
+
+app.get("/", (req,res) => {
+    const options = {
+        title: "hi!",
+        content: "this is our thing!", 
+        info: "yaaay"
+        //add the a tag here to get us to index2
+    };
+    res.render("index1", options);
+})
+
+app.listen(port, () => {
+  console.log(`server is listening on port: ${port}.`);  
+});
+
